043b57e66b6e2f629d05313a36e693124da9fa87
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2021-01-18 00:49:13 +0900

    Lighting improved

--- src/index.js
+++ src/index.js
@@ -7,16 +7,50 @@ import {
   Color,
 } from "three";
 import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js";
+// import { LightProbeGenerator } from 'three/examples/jsm/lights/LightProbeGenerator.js';
 import countries from "./files/globe-data.json";
 import EarthDarkSkin from "./files/earth-dark.jpg";
 var renderer, camera, scene, controls;
 var Globe;
 
 init();
+initGlobe();
 animate();
 
+// SECTION Initializing core elements
 function init() {
-  //   Initialize the Globe
+  // Initialize renderer
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.outputEncoding = THREE.sRGBEncoding;
+  document.body.appendChild(renderer.domElement);
+
+  // Initialize scene, light
+  scene = new Scene();
+  scene.add(new AmbientLight(0xbbbbbb, 0.4));
+  scene.background = new Color(0x040d21);
+  scene.add(new DirectionalLight(0xffffff, 0.8));
+
+  // Initialize camera
+  camera = new PerspectiveCamera();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  camera.position.z = 300;
+
+  // Initialize controls
+  controls = new TrackballControls(camera, renderer.domElement);
+  controls.dynamicDampingFactor = 0.1;
+  controls.noPan = true;
+  controls.minDistance = 300;
+  controls.maxDistance = 500;
+  controls.rotateSpeed = 1.5;
+  controls.zoomSpeed = 0.5;
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+function initGlobe() {
+  // Initialize the Globe
   Globe = new ThreeGlobe()
     // .globeImageUrl(EarthDarkSkin)
     .hexPolygonsData(countries.features)
@@ -31,32 +65,7 @@ function init() {
     );
 
   Globe.globeMaterial().color = new Color(0x7400b8);
-  // Initialize the scene, renderer, etc.
-  renderer = new WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  scene = new Scene();
   scene.add(Globe);
-  scene.add(new AmbientLight(0xbbbbbb));
-  // scene.add(new DirectionalLight(0xffffff, 0.6));
-
-  // Setup camera
-  camera = new PerspectiveCamera();
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  camera.position.z = 300;
-
-  // Add camera controls
-  controls = new TrackballControls(camera, renderer.domElement);
-  controls.dynamicDampingFactor = 0.1;
-  controls.noPan = true;
-  controls.minDistance = 300;
-  controls.maxDistance = 500;
-  controls.rotateSpeed = 1.5;
-  controls.zoomSpeed = 0.5;
-
-  window.addEventListener("resize", onWindowResize, false);
 }
 
 function onWindowResize() {
